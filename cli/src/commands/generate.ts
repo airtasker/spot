ca87b1e7cdc23422d4b763313cf1d405af61c522
Francois Wouts <f@zenc.io>
2018-10-25 09:03:41 +1100

    Clean up generator declarations in CLI

--- cli/src/commands/generate.ts
+++ cli/src/commands/generate.ts
@@ -10,6 +10,7 @@ import {
 } from "../../../lib/src/generators/typescript/express-server";
 import { generateTypesSource } from "../../../lib/src/generators/typescript/types";
 import { generateValidatorsSource } from "../../../lib/src/generators/typescript/validators";
+import { Api } from "../../../lib/src/models";
 import { parsePath } from "../../../lib/src/parser";
 
 export default class Generate extends Command {
@@ -52,80 +53,17 @@ Generated the following files:
     const { flags } = this.parse(Generate);
     const { api: apiPath, language, generator, out: outDir } = flags;
     const api = await parsePath(apiPath);
-    const generatedFiles: {
-      [relativePath: string]: string;
-    } = {};
-    switch (generator) {
-      case "json-schema":
-        switch (language) {
-          case "json":
-            generatedFiles["types.json"] = generateJsonSchema(api, "json");
-            break;
-          case "yaml":
-            generatedFiles["types.yaml"] = generateJsonSchema(api, "yaml");
-            break;
-          default:
-            this.error(
-              `Unsupported language for generator ${generator}: ${language}`
-            );
-            this.exit(1);
-        }
-        break;
-      case "openapi-3":
-        switch (language) {
-          case "json":
-            generatedFiles["api.json"] = generateOpenApiV3(api, "json");
-            break;
-          case "yaml":
-            generatedFiles["api.yaml"] = generateOpenApiV3(api, "yaml");
-            break;
-          default:
-            this.error(
-              `Unsupported language for generator ${generator}: ${language}`
-            );
-            this.exit(1);
-        }
-        break;
-      case "axios-client":
-        switch (language) {
-          case "typescript":
-            generatedFiles["types.ts"] = generateTypesSource(api);
-            generatedFiles["validators.ts"] = generateValidatorsSource(api);
-            generatedFiles["client.ts"] = generateAxiosClientSource(api);
-            break;
-          default:
-            this.error(
-              `Unsupported language for generator ${generator}: ${language}`
-            );
-            this.exit(1);
-        }
-        break;
-      case "express-server":
-        switch (language) {
-          case "typescript":
-            generatedFiles["types.ts"] = generateTypesSource(api);
-            generatedFiles["validators.ts"] = generateValidatorsSource(api);
-            generatedFiles["server.ts"] = generateExpressServerSource(api);
-            for (const [endpointName, endpoint] of Object.entries(
-              api.endpoints
-            )) {
-              generatedFiles[
-                `endpoints/${endpointName}.ts`
-              ] = generateEndpointHandlerSource(api, endpointName, endpoint);
-            }
-            break;
-          default:
-            this.error(
-              `Unsupported language for generator ${generator}: ${language}`
-            );
-            this.exit(1);
-        }
-        break;
-
-      default:
-        this.error(`Unsupported generator: ${generator}`);
-        this.exit(1);
+    if (!generators[generator]) {
+      this.error(`No such generator ${generator}`);
+      this.exit(1);
     }
+    if (!generators[generator][language]) {
+      this.error(
+        `Unsupported language for generator ${generator}: ${language}`
+      );
+      this.exit(1);
+    }
+    const generatedFiles = generators[generator][language](api);
     for (const [relativePath, content] of Object.entries(generatedFiles)) {
       outputFile(outDir, relativePath, content);
     }
@@ -135,6 +73,58 @@ Generated the following files:
     );
   }
 }
+
+const generators: {
+  [generatorName: string]: {
+    [languageName: string]: (
+      api: Api
+    ) => {
+      [relativePath: string]: string;
+    };
+  };
+} = {
+  "json-schema": {
+    json: api => ({
+      "types.json": generateJsonSchema(api, "json")
+    }),
+    yaml: api => ({
+      "types.yml": generateJsonSchema(api, "yaml")
+    })
+  },
+  "openapi-3": {
+    json: api => ({
+      "api.json": generateOpenApiV3(api, "json")
+    }),
+    yaml: api => ({
+      "api.yml": generateOpenApiV3(api, "yaml")
+    })
+  },
+  "axios-client": {
+    typescript: api => ({
+      "types.ts": generateTypesSource(api),
+      "validators.ts": generateValidatorsSource(api),
+      "client.ts": generateAxiosClientSource(api)
+    })
+  },
+  "express-server": {
+    typescript: api => ({
+      "types.ts": generateTypesSource(api),
+      "validators.ts": generateValidatorsSource(api),
+      "server.ts": generateExpressServerSource(api),
+      ...Object.entries(api.endpoints).reduce(
+        (acc, [endpointName, endpoint]) => {
+          acc[`endpoints/${endpointName}.ts`] = generateEndpointHandlerSource(
+            api,
+            endpointName,
+            endpoint
+          );
+          return acc;
+        },
+        {} as { [relativePath: string]: string }
+      )
+    })
+  }
+};
 
 function outputFile(outDir: string, relativePath: string, content: string) {
   const destinationPath = path.join(outDir, relativePath);
