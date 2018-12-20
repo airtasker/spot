0a0dbcdfa67946a056c8849ca8ea10bb68324df6
Francois Wouts <f@zenc.io>
2018-12-21 10:53:41 +1100

    Add comments for isRequestForEndpoint()

--- lib/src/mockserver/matcher.ts
+++ lib/src/mockserver/matcher.ts
@@ -3,6 +3,10 @@ import { Endpoint } from "../models";
 
 /**
  * Returns whether a given request should associated with an endpoint, as in the path and method match.
+ *
+ * @param req The incoming request.
+ * @param pathPrefix The path prefix on which the API should be served (e.g. /api/v2).
+ * @param endpoint The endpoint to match against.
  */
 export function isRequestForEndpoint(
   req: {
