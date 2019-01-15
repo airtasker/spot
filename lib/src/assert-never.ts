dee3c4aab04da0648a5331f8e5a0deeee4df6cdc
Francois Wouts <f@zenc.io>
2019-01-15 11:38:04 +1100

    Delete our version of assert-never

--- lib/src/assert-never.ts
+++
@@ -1,7 +0,0 @@
-// Equivalent to https://github.com/aikoven/assert-never.
-// Unfortunately importing it from node_modules causes issues.
-export default function assertNever(value: never): never {
-  throw new Error(
-    `Unhandled discriminated union member: ${JSON.stringify(value)}`
-  );
-}
