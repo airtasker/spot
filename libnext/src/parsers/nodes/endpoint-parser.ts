8315af7323597e1f122944bd2205934d2c51882c
Francois Wouts <f@zenc.io>
2019-01-15 15:19:54 +1100

    Clean up double merge

--- libnext/src/parsers/nodes/endpoint-parser.ts
+++ libnext/src/parsers/nodes/endpoint-parser.ts
@@ -1,18 +1,7 @@
 import { ClassDeclaration, ObjectLiteralExpression } from "ts-simple-ast";
 import { HttpMethod } from "../../models/http";
-import {
-  DynamicPathComponent,
-  EndpointNode,
-  PathComponent
-} from "../../models/nodes";
-import {
-  classMethodWithDecorator,
-  extractDecoratorFactoryConfiguration,
-  extractJsDocComment,
-  extractStringArrayProperty,
-  extractStringProperty,
-  isHttpMethod
-} from "../utilities/parser-utility";
+import { EndpointNode } from "../../models/nodes";
+import { classMethodWithDecorator, extractDecoratorFactoryConfiguration, extractJsDocComment, extractStringArrayProperty, extractStringProperty, isHttpMethod } from "../utilities/parser-utility";
 import { parseDefaultResponse } from "./default-response-parser";
 import { parseRequest } from "./request-parser";
 import { parseResponse } from "./response-parser";
@@ -29,7 +18,7 @@ export function parseEndpoint(klass: ClassDeclaration): EndpointNode {
   const tags = extractTagsProperty(configuration, "tags");
   const method = extractHttpMethodProperty(configuration, "method");
   const name = klass.getNameOrThrow();
-  const path = extractPathProperty(configuration, "path");
+  const path = extractStringProperty(configuration, "path");
   const requestMethod = classMethodWithDecorator(klass, "request");
   const request = requestMethod
     ? parseRequest(requestMethod)
@@ -67,19 +56,6 @@ export function parseEndpoint(klass: ClassDeclaration): EndpointNode {
 }
 
 /**
- * Extract a list of tags from an object literal.
- *
- * @param objectLiteral an object literal
- * @param propertyName the property to extract
- */
-function extractTagsProperty(
-  objectLiteral: ObjectLiteralExpression,
-  propertyName: string
-): string[] {
-  return extractStringArrayProperty(objectLiteral, propertyName);
-}
-
-/**
  * Extract a HTTP method property from an object literal.
  *
  * @param objectLiteral an object literal
@@ -97,57 +73,14 @@ function extractHttpMethodProperty(
 }
 
 /**
- * Extract a list of path components from a string path.
- *
- * An example of valid path is "/users/:id".
+ * Extract a list of tags from an object literal.
  *
  * @param objectLiteral an object literal
  * @param propertyName the property to extract
  */
-function extractPathProperty(
+function extractTagsProperty(
   objectLiteral: ObjectLiteralExpression,
   propertyName: string
-): PathComponent[] {
-  const stringPath = extractStringProperty(objectLiteral, propertyName);
-  const pathComponents: PathComponent[] = [];
-  if (stringPath.length > 0) {
-    let componentStartPosition = 0;
-    do {
-      if (stringPath.charAt(componentStartPosition) === ":") {
-        // The parameter name extends until a character that isn't a valid name.
-        const nextNonNamePositionRelative = stringPath
-          .substr(componentStartPosition + 1)
-          .search(/[^a-z0-9_]/gi);
-        const dynamicPathComponent: DynamicPathComponent = {
-          kind: "dynamic",
-          paramName: stringPath.substr(
-            componentStartPosition + 1,
-            nextNonNamePositionRelative === -1
-              ? undefined
-              : nextNonNamePositionRelative
-          )
-        };
-        pathComponents.push(dynamicPathComponent);
-        componentStartPosition =
-          nextNonNamePositionRelative === -1
-            ? -1
-            : componentStartPosition + 1 + nextNonNamePositionRelative;
-      } else {
-        // The static component extends until the next parameter, which starts with ":".
-        const nextColumnPosition = stringPath.indexOf(
-          ":",
-          componentStartPosition
-        );
-        pathComponents.push({
-          kind: "static",
-          content: stringPath.substring(
-            componentStartPosition,
-            nextColumnPosition === -1 ? undefined : nextColumnPosition
-          )
-        });
-        componentStartPosition = nextColumnPosition;
-      }
-    } while (componentStartPosition !== -1);
-  }
-  return pathComponents;
+): string[] {
+  return extractStringArrayProperty(objectLiteral, propertyName);
 }
