9dc87e46c13c2b5edd6aed22d21f152b53024575
Francois Wouts <f@zenc.io>
2019-01-21 12:28:24 +1100

    Implement mock server based on the new syntax

---
+++ libnext/src/mockserver/dummy.ts
@@ -0,0 +1,70 @@
+import assertNever from "assert-never";
+import { generate as generateRandomString } from "randomstring";
+import { TypeDefinition } from "../models/definitions";
+import { DataType, TypeKind } from "../models/types";
+
+/**
+ * Generates dummy data based on a type.
+ */
+export function generateData(types: TypeDefinition[], type: DataType): any {
+  switch (type.kind) {
+    case TypeKind.NULL:
+      return null;
+    case TypeKind.BOOLEAN:
+      return randomBoolean();
+    case TypeKind.BOOLEAN_LITERAL:
+      return type.value;
+    case TypeKind.STRING:
+      return generateRandomString();
+    case TypeKind.STRING_LITERAL:
+      return type.value;
+    case TypeKind.NUMBER:
+      return randomDouble(100);
+    case TypeKind.INTEGER:
+      return randomInteger(100);
+    case TypeKind.DATE:
+    case TypeKind.DATE_TIME:
+      return new Date().toISOString();
+    case TypeKind.NUMBER_LITERAL:
+      return type.value;
+    case TypeKind.OBJECT:
+      return type.properties.reduce<{ [key: string]: any }>((acc, property) => {
+        if (randomBoolean() || !property.optional) {
+          acc[property.name] = generateData(types, property.type);
+        }
+        return acc;
+      }, {});
+    case TypeKind.ARRAY:
+      const size = randomInteger(10);
+      const array: any[] = [];
+      for (let i = 0; i < size; i++) {
+        array.push(generateData(types, type.elements));
+      }
+      return array;
+    case TypeKind.UNION:
+      return generateData(
+        types,
+        type.types[randomInteger(type.types.length - 1)]
+      );
+    case TypeKind.TYPE_REFERENCE:
+      const referencedType = types.find(t => t.name === type.name);
+      if (!referencedType) {
+        throw new Error(`Missing referenced type: ${type.name}`);
+      }
+      return generateData(types, referencedType.type);
+    default:
+      throw assertNever(type);
+  }
+}
+
+function randomBoolean() {
+  return Math.random() > 0.5;
+}
+
+function randomInteger(max: number) {
+  return Math.round(randomDouble(max));
+}
+
+function randomDouble(max: number) {
+  return Math.random() * max;
+}
---
+++ libnext/src/mockserver/matcher.spec.ts
@@ -0,0 +1,153 @@
+import { isRequestForEndpoint } from "./matcher";
+
+const BASE_ENDPOINT = {
+  name: "my-endpoint",
+  description: "",
+  path: "",
+  request: {
+    headers: [],
+    pathParams: [],
+    queryParams: []
+  },
+  responses: [],
+  tags: []
+};
+
+describe("Matcher", () => {
+  describe("isRequestForEndpoint", () => {
+    it("identifies correct matches", () => {
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "get",
+            path: "/users"
+          },
+          "",
+          {
+            ...BASE_ENDPOINT,
+            method: "GET",
+            path: "/users"
+          }
+        )
+      ).toBeTruthy();
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "get",
+            path: "/api/v2/users"
+          },
+          "/api/v2",
+          {
+            ...BASE_ENDPOINT,
+            method: "GET",
+            path: "/users"
+          }
+        )
+      ).toBeTruthy();
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "post",
+            path: "/users/123"
+          },
+          "",
+          {
+            ...BASE_ENDPOINT,
+            method: "POST",
+            path: "/users/:userId"
+          }
+        )
+      ).toBeTruthy();
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "post",
+            path: "/users/123/details"
+          },
+          "",
+          {
+            ...BASE_ENDPOINT,
+            method: "POST",
+            path: "/users/:userId/details"
+          }
+        )
+      ).toBeTruthy();
+    });
+    it("rejects wrong method", () => {
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "POST",
+            path: "/users"
+          },
+          "",
+          {
+            ...BASE_ENDPOINT,
+            method: "GET",
+            path: "/users"
+          }
+        )
+      ).toBeFalsy();
+    });
+    it("rejects wrong prefix", () => {
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "POST",
+            path: "/users"
+          },
+          "/api/v2",
+          {
+            ...BASE_ENDPOINT,
+            method: "GET",
+            path: "/users"
+          }
+        )
+      ).toBeFalsy();
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "POST",
+            path: "/api/v3/users"
+          },
+          "/api/v2",
+          {
+            ...BASE_ENDPOINT,
+            method: "GET",
+            path: "/users"
+          }
+        )
+      ).toBeFalsy();
+    });
+    it("rejects wrong path", () => {
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "GET",
+            path: "/users/abc"
+          },
+          "",
+          {
+            ...BASE_ENDPOINT,
+            method: "GET",
+            path: "/users"
+          }
+        )
+      ).toBeFalsy();
+      expect(
+        isRequestForEndpoint(
+          {
+            method: "GET",
+            path: "/users"
+          },
+          "",
+          {
+            ...BASE_ENDPOINT,
+            method: "GET",
+            path: "/users/abc"
+          }
+        )
+      ).toBeFalsy();
+    });
+  });
+});
