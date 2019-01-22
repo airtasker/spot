da077461907371de96cba6e4ae8d080ef40d68b3
Leslie Fung <leslie.fung.lf@gmail.com>
2019-01-22 13:54:30 +1100

    fixed changed parser to resolve @airtasker/spot to internal files

--- lib/src/parsers/parser.spec.ts
+++ lib/src/parsers/parser.spec.ts
@@ -2,12 +2,7 @@ import { parse } from "./parser";
 
 describe("parser", () => {
   test("parses all information", () => {
-    const result = parse("./lib/src/test/examples/contract.ts", {
-      baseUrl: "./",
-      paths: {
-        "@airtasker/spot": ["./lib/src/lib"]
-      }
-    });
+    const result = parse("./lib/src/test/examples/contract.ts");
     expect(result.api).not.toBeUndefined;
     expect(result.endpoints).toHaveLength(2);
     expect(result.types).toHaveLength(7);
