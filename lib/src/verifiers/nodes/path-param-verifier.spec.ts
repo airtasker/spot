0c5b0101b4da0429fe874d221f71319656750fd9
François Wouts <f@zenc.io>
2019-02-22 13:57:58 +1100

    Add support for complex query (object & array) and path params (array only) (#110)
    QueryParams (array): `style: pipeDelimited` & `explode: true`
    QueryParams (object): `style: deepObject` & `explode: true`
    PathParams (array): `style: simple` & `explode: false` - omitted as it is the default
    PathParams (object): * NO SUPPORT*
    
    We also check that arrays in query or path params are only of primitive types.

--- lib/src/verifiers/nodes/path-param-verifier.spec.ts
+++ lib/src/verifiers/nodes/path-param-verifier.spec.ts
@@ -4,7 +4,7 @@ import { fakeLocatable } from "../../test/fake-locatable";
 import { verifyPathParamNode } from "./path-param-verifier";
 
 describe("path param node verifier", () => {
-  test("valid for correct usage", () => {
+  test("valid for URL-safe type", () => {
     const pathParamNode: PathParamNode = {
       name: fakeLocatable("somename"),
       type: {
@@ -14,25 +14,92 @@ describe("path param node verifier", () => {
     expect(verifyPathParamNode(pathParamNode, [])).toHaveLength(0);
   });
 
-  test("invalid for incorrect usage", () => {
+  test("valid for URL-safe array type", () => {
+    const pathParamNode: PathParamNode = {
+      name: fakeLocatable("somename"),
+      type: {
+        kind: TypeKind.ARRAY,
+        elements: {
+          kind: TypeKind.STRING
+        }
+      }
+    };
+    expect(verifyPathParamNode(pathParamNode, [])).toHaveLength(0);
+  });
+
+  test("invalid for invalid name", () => {
     const pathParamNode: PathParamNode = {
       name: fakeLocatable("so$men ame"),
+      type: {
+        kind: TypeKind.STRING
+      }
+    };
+    const errors = verifyPathParamNode(pathParamNode, []);
+    expect(errors).toMatchObject([
+      {
+        message:
+          "path param name may only contain alphanumeric, underscore and hyphen characters"
+      }
+    ]);
+  });
+
+  test("invalid for URL-unsafe type", () => {
+    const pathParamNode: PathParamNode = {
+      name: fakeLocatable("somename"),
       type: {
         kind: TypeKind.NULL
       }
     };
     const errors = verifyPathParamNode(pathParamNode, []);
-    expect(errors).toHaveLength(2);
-    expect(errors).toContainEqual({
-      message:
-        "path param name may only contain alphanumeric, underscore and hyphen characters",
-      location: "somelocation.ts",
-      line: 4
-    });
-    expect(errors).toContainEqual({
-      message: "path param type may only stem from string or number types",
-      location: "somelocation.ts",
-      line: 4
-    });
+    expect(errors).toMatchObject([
+      {
+        message:
+          "path param type may only be a URL-safe type or an array of URL-safe types"
+      }
+    ]);
+  });
+
+  test("invalid for URL-unsafe array type", () => {
+    const pathParamNode: PathParamNode = {
+      name: fakeLocatable("somename"),
+      type: {
+        kind: TypeKind.ARRAY,
+        elements: {
+          kind: TypeKind.NULL
+        }
+      }
+    };
+    const errors = verifyPathParamNode(pathParamNode, []);
+    expect(errors).toMatchObject([
+      {
+        message:
+          "path param type may only be a URL-safe type or an array of URL-safe types"
+      }
+    ]);
+  });
+
+  test("invalid for objects", () => {
+    const pathParamNode: PathParamNode = {
+      name: fakeLocatable("somename"),
+      type: {
+        kind: TypeKind.OBJECT,
+        properties: [
+          {
+            name: "name",
+            optional: false,
+            type: {
+              kind: TypeKind.STRING
+            }
+          }
+        ]
+      }
+    };
+    const errors = verifyPathParamNode(pathParamNode, []);
+    expect(errors).toMatchObject([
+      {
+        message:
+          "path param type may only be a URL-safe type or an array of URL-safe types"
+      }
+    ]);
   });
 });
