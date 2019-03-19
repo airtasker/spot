f05f2cecc43c419d1b6c747536e1350e0ca1b3e6
Leslie Fung <leslie.fung.lf@gmail.com>
2019-03-20 10:19:09 +1100

    tslint change

--- cli/src/test-utils/test-logger.ts
+++ cli/src/test-utils/test-logger.ts
@@ -8,27 +8,24 @@ export const TestLogger = {
   warn
 };
 
+// tslint:disable:no-console
 function error(message: string) {
-  // tslint:disable-next-line: no-console
   console.log(chalk.red(message));
 }
 
 function log(message: string) {
-  // tslint:disable-next-line: no-console
   console.log(chalk.white(message));
 }
 
 function mute(message: string) {
-  // tslint:disable-next-line: no-console
   console.log(chalk.grey(message));
 }
 
 function success(message: string) {
-  // tslint:disable-next-line: no-console
   console.log(chalk.green(message));
 }
 
 function warn(message: string) {
-  // tslint:disable-next-line: no-console
   console.log(chalk.yellow(message));
 }
+// tslint:enable:no-console
