864e3837547682fe2819356aa5d4f99948bcd73d
François Wouts <f@zenc.io>
2019-03-20 17:24:52 +1100

    Add support for exports (#143)

--- lib/src/parsers/parser.spec.ts
+++ lib/src/parsers/parser.spec.ts
@@ -8,12 +8,11 @@ describe("parser", () => {
     expect(result.types).toHaveLength(7);
   });
 
-  test("follows recursive imports", () => {
+  test("follows recursive imports and exports", () => {
     const result = parse(
       "./lib/src/test/examples/recursive-imports-and-exports/contract.ts"
     );
     expect(result.api).not.toBeUndefined;
-    // TODO: Support exports as well.
-    expect(result.endpoints).toHaveLength(2);
+    expect(result.endpoints).toHaveLength(4);
   });
 });
