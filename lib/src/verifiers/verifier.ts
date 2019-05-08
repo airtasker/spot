b687e7903b7649363cc328bab1e4fa5ab79b2a0e
Francois Wouts <f@zenc.io>
2019-05-08 15:42:00 +1000

    Add type unicity check

--- lib/src/verifiers/verifier.ts
+++ lib/src/verifiers/verifier.ts
@@ -24,5 +24,16 @@ export function verify(contract: ContractNode): VerificationError[] {
     });
   }
 
+  // ensure types have unique names
+  const typeNames = contract.types.map(type => type.name);
+  if (uniq(typeNames).length !== typeNames.length) {
+    errors.push({
+      message: "types must have unique names",
+      // TODO: use a duplicated type location
+      location: contract.api.location,
+      line: contract.api.line
+    });
+  }
+
   return errors;
 }
