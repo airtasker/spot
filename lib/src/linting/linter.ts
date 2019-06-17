dba583ae703cf58061b703b569cc641ac5e06398
François Wouts <f@zenc.io>
2019-06-17 13:06:13 +1000

    Remove usage of ramda (#285)
    We have both lodash and ramda. They do the same job, but lodash is more widely used in the codebase. Therefore, this removes all usage of ramda.

--- lib/src/linting/linter.ts
+++ lib/src/linting/linter.ts
@@ -1,4 +1,4 @@
-import { unnest } from "ramda";
+import { flatten } from "lodash";
 import { ContractNode } from "../models/nodes";
 import { LintingRuleViolation } from "./rule";
 import { availableRules, RuleName } from "./rules";
@@ -7,7 +7,7 @@ export function lint(
   contract: ContractNode,
   ruleNames: RuleName[] = Object.keys(availableRules) as RuleName[]
 ): LintingRuleViolation[] {
-  return unnest(
+  return flatten(
     ruleNames
       .map(ruleName => availableRules[ruleName])
       .map(rule => rule(contract))
