575118fc9846cce90d53ac1fb18259e750d4db3b
Francois Wouts <f@zenc.io>
2019-06-17 15:49:14 +1000

    Remove TODO

--- lib/src/linting/rules.ts
+++ lib/src/linting/rules.ts
@@ -6,7 +6,6 @@ export const availableRules = {
   "has-request-payload": hasRequestPayload,
   "has-response-payload": hasResponsePayload,
   "has-discriminator": hasDiscriminator
-  // TODO: Second rule to ensure no nested types in unions.
 };
 
 export type RuleName = keyof typeof availableRules;
