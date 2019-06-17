2a4d97134ce3a8867b79700b26cb6df184ce3e43
Francois Wouts <f@zenc.io>
2019-06-17 14:49:24 +1000

    Revert "Remove rule (separate PR)"
    This reverts commit 308db953bbb5073d00bff9df112236a054695f56.

---
+++ lib/src/linting/rules/has-discriminator.spec.ts
@@ -0,0 +1,295 @@
+import { HttpMethod } from "../../models/http";
+import { ApiNode, BodyNode, DefaultResponseNode, EndpointNode, RequestNode, ResponseNode } from "../../models/nodes";
+import { arrayType, objectType, STRING, stringLiteral, unionType } from "../../models/types";
+import { fakeLocatable } from "../../spec-helpers/fake-locatable";
+import { hasDiscriminator } from "./has-discriminator";
+
+describe("rule: has-discriminator", () => {
+  test("valid for correct usage", () => {
+    const errors = hasDiscriminator({
+      api: fakeLocatable<ApiNode>({
+        name: fakeLocatable("example-api")
+      }),
+      endpoints: [
+        fakeLocatable<EndpointNode>({
+          name: fakeLocatable("createUser"),
+          method: fakeLocatable<HttpMethod>("POST"),
+          path: fakeLocatable("/users"),
+          request: fakeLocatable<RequestNode>({
+            body: fakeLocatable<BodyNode>({
+              type: unionType([
+                objectType([
+                  {
+                    name: "type",
+                    optional: false,
+                    type: stringLiteral("user")
+                  }
+                ]),
+                objectType([
+                  {
+                    name: "type",
+                    optional: false,
+                    type: stringLiteral("admin")
+                  }
+                ])
+              ])
+            })
+          }),
+          tests: [],
+          responses: [
+            fakeLocatable<ResponseNode>({
+              status: fakeLocatable(200),
+              body: fakeLocatable<BodyNode>({
+                type: unionType([
+                  objectType([
+                    {
+                      name: "type",
+                      optional: false,
+                      type: stringLiteral("user")
+                    }
+                  ]),
+                  objectType([
+                    {
+                      name: "type",
+                      optional: false,
+                      type: stringLiteral("admin")
+                    }
+                  ])
+                ])
+              })
+            })
+          ]
+        })
+      ],
+      types: []
+    });
+    expect(errors).toEqual([]);
+  });
+
+  test("rejects missing discriminator in request", () => {
+    const errors = hasDiscriminator({
+      api: fakeLocatable<ApiNode>({
+        name: fakeLocatable("example-api")
+      }),
+      endpoints: [
+        fakeLocatable<EndpointNode>({
+          name: fakeLocatable("createUser"),
+          method: fakeLocatable<HttpMethod>("POST"),
+          path: fakeLocatable("/users"),
+          request: fakeLocatable<RequestNode>({
+            body: fakeLocatable<BodyNode>({
+              type: unionType([
+                objectType([
+                  {
+                    name: "type",
+                    optional: false,
+                    type: stringLiteral("user")
+                  }
+                ]),
+                STRING
+              ])
+            })
+          }),
+          tests: [],
+          responses: []
+        })
+      ],
+      types: []
+    });
+    expect(errors).toEqual([
+      {
+        message:
+          "The type `createUser (request body)` doesn't have a discriminator"
+      }
+    ]);
+  });
+
+  test("rejects missing discriminator in default response", () => {
+    const errors = hasDiscriminator({
+      api: fakeLocatable<ApiNode>({
+        name: fakeLocatable("example-api")
+      }),
+      endpoints: [
+        fakeLocatable<EndpointNode>({
+          name: fakeLocatable("createUser"),
+          method: fakeLocatable<HttpMethod>("POST"),
+          path: fakeLocatable("/users"),
+          tests: [],
+          defaultResponse: fakeLocatable<DefaultResponseNode>({
+            body: fakeLocatable<BodyNode>({
+              type: unionType([
+                objectType([
+                  {
+                    name: "type",
+                    optional: false,
+                    type: stringLiteral("user")
+                  }
+                ]),
+                STRING
+              ])
+            })
+          }),
+          responses: []
+        })
+      ],
+      types: []
+    });
+    expect(errors).toEqual([
+      {
+        message:
+          "The type `createUser (default response body)` doesn't have a discriminator"
+      }
+    ]);
+  });
+
+  test("rejects missing discriminator in specific response", () => {
+    const errors = hasDiscriminator({
+      api: fakeLocatable<ApiNode>({
+        name: fakeLocatable("example-api")
+      }),
+      endpoints: [
+        fakeLocatable<EndpointNode>({
+          name: fakeLocatable("createUser"),
+          method: fakeLocatable<HttpMethod>("POST"),
+          path: fakeLocatable("/users"),
+          tests: [],
+          responses: [
+            fakeLocatable<ResponseNode>({
+              status: fakeLocatable(200),
+              body: fakeLocatable<BodyNode>({
+                type: unionType([
+                  objectType([
+                    {
+                      name: "type",
+                      optional: false,
+                      type: stringLiteral("user")
+                    }
+                  ]),
+                  STRING
+                ])
+              })
+            })
+          ]
+        })
+      ],
+      types: []
+    });
+    expect(errors).toEqual([
+      {
+        message:
+          "The type `createUser (response body for status 200)` doesn't have a discriminator"
+      }
+    ]);
+  });
+
+  test("rejects missing discriminator nested inside array type", () => {
+    const errors = hasDiscriminator({
+      api: fakeLocatable<ApiNode>({
+        name: fakeLocatable("example-api")
+      }),
+      endpoints: [],
+      types: [
+        {
+          name: "UserOrAdminList",
+          type: arrayType(
+            unionType([
+              objectType([
+                {
+                  name: "type",
+                  optional: false,
+                  type: stringLiteral("user")
+                }
+              ]),
+              STRING
+            ])
+          )
+        }
+      ]
+    });
+    expect(errors).toEqual([
+      {
+        message:
+          "The type `UserOrAdminList` doesn't have a discriminator"
+      }
+    ]);
+  });
+
+  test("rejects missing discriminator nested inside object type", () => {
+    const errors = hasDiscriminator({
+      api: fakeLocatable<ApiNode>({
+        name: fakeLocatable("example-api")
+      }),
+      endpoints: [],
+      types: [
+        {
+          name: "UserOrAdminContainer",
+          type: objectType([
+            {
+              name: "nested",
+              optional: false,
+              type: unionType([
+                objectType([
+                  {
+                    name: "type",
+                    optional: false,
+                    type: stringLiteral("user")
+                  }
+                ]),
+                STRING
+              ])
+            }
+          ])
+        }
+      ]
+    });
+    expect(errors).toEqual([
+      {
+        message:
+          "The type `UserOrAdminContainer.nested` doesn't have a discriminator"
+      }
+    ]);
+  });
+
+  test("rejects missing discriminator nested inside union type", () => {
+    const errors = hasDiscriminator({
+      api: fakeLocatable<ApiNode>({
+        name: fakeLocatable("example-api")
+      }),
+      endpoints: [],
+      types: [
+        {
+          name: "UserOrAdminUnionWrapper",
+          type: unionType([
+            objectType([
+              {
+                name: "type",
+                optional: false,
+                type: stringLiteral("wrapper")
+              },
+              {
+                name: "value",
+                optional: false,
+                type: unionType([
+                  objectType([
+                    {
+                      name: "type",
+                      optional: false,
+                      type: stringLiteral("user")
+                    }
+                  ]),
+                  STRING
+                ])
+              }
+            ]),
+          ])
+        }
+      ]
+    });
+    expect(errors).toEqual([
+      {
+        message:
+          "The type `UserOrAdminUnionWrapper[0].value` doesn't have a discriminator"
+      }
+    ]);
+  });
+});
