308db953bbb5073d00bff9df112236a054695f56
Francois Wouts <f@zenc.io>
2019-06-17 14:48:25 +1000

    Remove rule (separate PR)

--- lib/src/linting/rules.ts
+++ lib/src/linting/rules.ts
@@ -1,12 +1,9 @@
-import { hasDiscriminator } from "./rules/has-discriminator";
 import { hasRequestPayload } from "./rules/has-request-payload";
 import { hasResponsePayload } from "./rules/has-response-payload";
 
 export const availableRules = {
   "has-request-payload": hasRequestPayload,
   "has-response-payload": hasResponsePayload,
-  "has-discriminator": hasDiscriminator
-  // TODO: Second rule to ensure no nested types in unions.
 };
 
 export type RuleName = keyof typeof availableRules;
