691e8bcf8f7d3565c59b0a6b12c60a5a35ed0ed0
François Wouts <f@zenc.io>
2019-06-18 15:54:01 +1000

    Add a lint rule to enforce union types have a discriminator (#288)

--- lib/src/linting/rules.ts
+++ lib/src/linting/rules.ts
@@ -1,9 +1,11 @@
+import { hasDiscriminator } from "./rules/has-discriminator";
 import { hasRequestPayload } from "./rules/has-request-payload";
 import { hasResponsePayload } from "./rules/has-response-payload";
 
 export const availableRules = {
   "has-request-payload": hasRequestPayload,
-  "has-response-payload": hasResponsePayload
+  "has-response-payload": hasResponsePayload,
+  "has-discriminator": hasDiscriminator
 };
 
 export type RuleName = keyof typeof availableRules;
