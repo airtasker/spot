5519005bafaa3f15f704eda480b30287a8b34a0d
Francois Wouts <f@zenc.io>
2019-06-19 11:41:37 +1000

    Run tslint

--- lib/src/linting/rules/has-discriminator.ts
+++ lib/src/linting/rules/has-discriminator.ts
@@ -1,9 +1,9 @@
 import { flatten } from "lodash";
 import { DataType, UnionType } from "../../models/types";
+import { extractEndpointTypes } from "../../utilities/extract-endpoint-types";
+import { extractNestedUnionTypes } from "../../utilities/extract-union-types";
 import { inferDiscriminator } from "../../utilities/infer-discriminator";
 import { LintingRule } from "../rule";
-import { extractNestedUnionTypes } from "../../utilities/extract-union-types";
-import { extractEndpointTypes } from "../../utilities/extract-endpoint-types";
 
 /**
  * Checks that all union types have a discriminator.
@@ -18,8 +18,7 @@ export const hasDiscriminator: LintingRule = contract => {
   );
   return unionTypes
     .filter(
-      typeNode =>
-        inferDiscriminator(contract.types, typeNode.type) === null
+      typeNode => inferDiscriminator(contract.types, typeNode.type) === null
     )
     .map(typeNode => ({
       message: `The type \`${typeNode.name}\` doesn't have a discriminator`
