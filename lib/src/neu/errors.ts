ebf3382d7505cf4fc34bad17058f77b982069787
Leslie Fung <leslie.fung.lf@gmail.com>
2019-09-06 16:21:25 +1000

    removing things jes doesn't like

--- lib/src/neu/errors.ts
+++ lib/src/neu/errors.ts
@@ -16,58 +16,6 @@ export class ParserError extends Error {
 export class OptionalNotAllowedError extends ParserError {}
 export class TypeNotAllowedError extends ParserError {}
 
-export function printParserError(
-  err: ParserError,
-  project: Project,
-  options?: Partial<PrintOpts>
-): string {
-  const opts = {
-    surroundingLines: (options || {}).surroundingLines || 3
-  };
-
-  return (
-    `${err.message}:\n\n` +
-    err.locations
-      .map(l => printParserErrorLocation(l, project, opts))
-      .join("\n\n")
-  );
-}
-
-function printParserErrorLocation(
-  location: {
-    file: string;
-    position: number;
-  },
-  project: Project,
-  opts: PrintOpts
-): string {
-  const sourceFile = project.getSourceFileOrThrow(location.file);
-  const line = sourceFile.getLineAndColumnAtPos(location.position).line;
-
-  const splittedSource = sourceFile.getFullText().split("\n");
-  const totalLines = splittedSource.length;
-
-  const lowerBound =
-    line - opts.surroundingLines >= 1 ? line - opts.surroundingLines : 1;
-  const upperBound =
-    line + opts.surroundingLines <= totalLines
-      ? line + opts.surroundingLines
-      : totalLines;
-  const lineNumberPadding = upperBound.toString().length;
-
-  return (
-    `${location.file}\n\n` +
-    splittedSource
-      .map((e, i) =>
-        i + 1 === line
-          ? `${(i + 1).toString().padStart(lineNumberPadding)} --> ${e}`
-          : `${(i + 1).toString().padStart(lineNumberPadding)}     ${e}`
-      )
-      .slice(lowerBound - 1, upperBound)
-      .join("\n")
-  );
-}
-
 interface PrintOpts {
   surroundingLines: number;
 }
