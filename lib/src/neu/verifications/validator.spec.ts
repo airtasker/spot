a2dbf35115e5100bedaf647d8be2dbef450f8778
medric <medric.chouan@gmail.com>
2019-09-21 16:26:59 +1000

    implement recursive types validators

--- lib/src/neu/verifications/validator.spec.ts
+++ lib/src/neu/verifications/validator.spec.ts
@@ -1,15 +1,40 @@
-import { floatType, Type } from "../types";
-import { Validator } from './validator';
+import { arrayType, booleanType, floatType, Type, int64Type } from "../types";
+import { Validator } from "./validator";
 
 describe("validators", () => {
-let validator: Validator;
-  beforeAll(() => {
+  let validator: Validator;
+  beforeEach(() => {
     validator = new Validator();
   });
 
-  test("should return an error when primitive type is incorrect", () => {
-    const result = validator.run({ name: 'param', value: 'notANumber' }, floatType());
-    expect(result).toBe(false);
-    expect(validator.messages[0]).toEqual('"param" should be float')
-  })
+  describe("valid inputs", () => {
+    test("should return true when a primitive value is value", () => {
+      const result = validator.run(
+        { name: "param", value: "true" },
+        booleanType()
+      );
+      expect(result).toBe(true);
+      expect(validator.messages.length).toEqual(0);
+    });
+  });
+
+  describe("invalid inputs", () => {
+    test("should return an error when a primitive value is invalid", () => {
+      const result = validator.run(
+        { name: "param", value: "notANumber" },
+        floatType()
+      );
+      expect(result).toBe(false);
+      expect(validator.messages[0]).toEqual('"param" should be float');
+    });
+
+    test("should return an error when a array of primitives is invalid", () => {
+      const result = validator.run(
+        { name: "param", value: ["1", "notANumber", "3"] },
+        arrayType(int64Type())
+      );
+      expect(result).toBe(false);
+      expect(validator.messages[0]).toEqual('"param[1]" should be int64');
+    });
+  });
 });
