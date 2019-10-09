a181809cfa91c7d7d4672de3113340421e0353b2
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2019-10-09 14:58:09 +1100

    TT-528 Add BaseError and make UnprocessableEntityError an extend it

--- lib/src/validation-server/spots/utils.ts
+++ lib/src/validation-server/spots/utils.ts
@@ -1,8 +1,12 @@
 import { String } from "../../lib";
 
-export interface UnprocessableEntityError {
+export interface BaseError {
   error_code: String;
   error_messages: String[];
+}
+
+export interface UnprocessableEntityError extends BaseError {
+  type: "unprocessable_entity";
 }
 
 export type Headers = String[];
