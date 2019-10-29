4575d62ecddaf304c0e34038a9f79cae9b828d99
Leslie Fung <leslie.fung.lf@gmail.com>
2019-10-30 10:07:26 +1100

    updated some snapshots

--- lib/src/neu/generators/json-schema-generator.spec.ts
+++ lib/src/neu/generators/json-schema-generator.spec.ts
@@ -32,127 +32,128 @@ describe("JSON Schema generator", () => {
   describe("generates type validator", () => {
     test("null", () => {
       expect(generateJsonSchemaType(nullType())).toMatchInlineSnapshot(`
-Object {
-  "type": "null",
-}
-`);
+        Object {
+          "type": "null",
+        }
+      `);
     });
 
     test("boolean", () => {
       expect(generateJsonSchemaType(booleanType())).toMatchInlineSnapshot(`
-Object {
-  "type": "boolean",
-}
-`);
+        Object {
+          "type": "boolean",
+        }
+      `);
     });
 
     test("boolean literal", () => {
       expect(generateJsonSchemaType(booleanLiteralType(true)))
         .toMatchInlineSnapshot(`
-Object {
-  "const": true,
-  "type": "boolean",
-}
-`);
+        Object {
+          "const": true,
+          "type": "boolean",
+        }
+      `);
       expect(generateJsonSchemaType(booleanLiteralType(false)))
         .toMatchInlineSnapshot(`
-Object {
-  "const": false,
-  "type": "boolean",
-}
-`);
+        Object {
+          "const": false,
+          "type": "boolean",
+        }
+      `);
     });
 
     test("string", () => {
       expect(generateJsonSchemaType(stringType())).toMatchInlineSnapshot(`
-Object {
-  "type": "string",
-}
-`);
+        Object {
+          "type": "string",
+        }
+      `);
     });
 
     test("string literal", () => {
       expect(generateJsonSchemaType(stringLiteralType("some literal")))
         .toMatchInlineSnapshot(`
-Object {
-  "const": "some literal",
-  "type": "string",
-}
-`);
+        Object {
+          "const": "some literal",
+          "type": "string",
+        }
+      `);
     });
 
     test("float", () => {
       expect(generateJsonSchemaType(floatType())).toMatchInlineSnapshot(`
-Object {
-  "type": "number",
-}
-`);
+        Object {
+          "type": "number",
+        }
+      `);
     });
 
     test("number literal", () => {
       expect(generateJsonSchemaType(floatLiteralType(1.5)))
         .toMatchInlineSnapshot(`
-Object {
-  "const": 1.5,
-  "type": "number",
-}
-`);
+        Object {
+          "const": 1.5,
+          "type": "number",
+        }
+      `);
       expect(generateJsonSchemaType(floatLiteralType(-23.1)))
         .toMatchInlineSnapshot(`
-Object {
-  "const": -23.1,
-  "type": "number",
-}
-`);
+        Object {
+          "const": -23.1,
+          "type": "number",
+        }
+      `);
     });
 
     test("int32", () => {
       expect(generateJsonSchemaType(int32Type())).toMatchInlineSnapshot(`
-Object {
-  "type": "integer",
-}
-`);
+        Object {
+          "type": "integer",
+        }
+      `);
     });
 
     test("int64", () => {
       expect(generateJsonSchemaType(int64Type())).toMatchInlineSnapshot(`
-Object {
-  "type": "integer",
-}
-`);
+        Object {
+          "type": "integer",
+        }
+      `);
     });
 
     test("number literal", () => {
       expect(generateJsonSchemaType(intLiteralType(0))).toMatchInlineSnapshot(`
-Object {
-  "const": 0,
-  "type": "integer",
-}
-`);
+        Object {
+          "const": 0,
+          "type": "integer",
+        }
+      `);
       expect(generateJsonSchemaType(intLiteralType(123)))
         .toMatchInlineSnapshot(`
-Object {
-  "const": 123,
-  "type": "integer",
-}
-`);
+        Object {
+          "const": 123,
+          "type": "integer",
+        }
+      `);
       expect(generateJsonSchemaType(intLiteralType(-1000)))
         .toMatchInlineSnapshot(`
-Object {
-  "const": -1000,
-  "type": "integer",
-}
-`);
+        Object {
+          "const": -1000,
+          "type": "integer",
+        }
+      `);
     });
 
     test("object", () => {
       expect(generateJsonSchemaType(objectType([]))).toMatchInlineSnapshot(`
-Object {
-  "properties": Object {},
-  "required": Array [],
-  "type": "object",
-}
-`);
+        Object {
+          "additionalProperties": true,
+          "properties": Object {},
+          "required": Array [],
+          "type": "object",
+        }
+      `);
       expect(
         generateJsonSchemaType(
           objectType([
@@ -164,18 +165,19 @@ Object {
           ])
         )
       ).toMatchInlineSnapshot(`
-Object {
-  "properties": Object {
-    "singleField": Object {
-      "type": "number",
-    },
-  },
-  "required": Array [
-    "singleField",
-  ],
-  "type": "object",
-}
-`);
+        Object {
+          "additionalProperties": true,
+          "properties": Object {
+            "singleField": Object {
+              "type": "number",
+            },
+          },
+          "required": Array [
+            "singleField",
+          ],
+          "type": "object",
+        }
+      `);
       expect(
         generateJsonSchemaType(
           objectType([
@@ -197,37 +199,38 @@ Object {
           ])
         )
       ).toMatchInlineSnapshot(`
-Object {
-  "properties": Object {
-    "field1": Object {
-      "type": "number",
-    },
-    "field2": Object {
-      "type": "string",
-    },
-    "field3": Object {
-      "type": "boolean",
-    },
-  },
-  "required": Array [
-    "field1",
-    "field2",
-  ],
-  "type": "object",
-}
-`);
+        Object {
+          "additionalProperties": true,
+          "properties": Object {
+            "field1": Object {
+              "type": "number",
+            },
+            "field2": Object {
+              "type": "string",
+            },
+            "field3": Object {
+              "type": "boolean",
+            },
+          },
+          "required": Array [
+            "field1",
+            "field2",
+          ],
+          "type": "object",
+        }
+      `);
     });
 
     test("array", () => {
       expect(generateJsonSchemaType(arrayType(stringType())))
         .toMatchInlineSnapshot(`
-Object {
-  "items": Object {
-    "type": "string",
-  },
-  "type": "array",
-}
-`);
+        Object {
+          "items": Object {
+            "type": "string",
+          },
+          "type": "array",
+        }
+      `);
     });
 
     test("union", () => {
@@ -236,29 +239,29 @@ Object {
           unionType([stringType(), floatType(), booleanType()])
         )
       ).toMatchInlineSnapshot(`
-Object {
-  "oneOf": Array [
-    Object {
-      "type": "string",
-    },
-    Object {
-      "type": "number",
-    },
-    Object {
-      "type": "boolean",
-    },
-  ],
-}
-`);
+        Object {
+          "oneOf": Array [
+            Object {
+              "type": "string",
+            },
+            Object {
+              "type": "number",
+            },
+            Object {
+              "type": "boolean",
+            },
+          ],
+        }
+      `);
     });
 
     test("type reference", () => {
       expect(generateJsonSchemaType(referenceType("OtherType")))
         .toMatchInlineSnapshot(`
-Object {
-  "$ref": "#/definitions/OtherType",
-}
-`);
+        Object {
+          "$ref": "#/definitions/OtherType",
+        }
+      `);
     });
   });
 });
