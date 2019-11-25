d30a6607ebc6baf1141264b7f7aa6933d96d8460
Leslie Fung <leslie.fung.lf@gmail.com>
2019-11-25 16:17:56 +1100

    migrated checksum to neu namespace

---
+++ lib/src/neu/checksum/hash.spec.ts
@@ -0,0 +1,57 @@
+import { createProjectFromExistingSourceFile } from "../../spec-helpers/helper";
+import { Endpoint } from "../definitions";
+import { parseContract } from "../parsers/contract-parser";
+import { TypeKind } from "../types";
+import { hashContract } from "./hash";
+
+describe("Hash", () => {
+  describe("hashContract", () => {
+    it("returns a consistent hash", () => {
+      const file = createProjectFromExistingSourceFile(
+        `${__dirname}/__spec-examples__/contract.ts`
+      ).file;
+
+      const { contract } = parseContract(file).unwrapOrThrow();
+
+      const hash0 = hashContract(contract);
+      const hash1 = hashContract(contract);
+
+      expect(hash0).toEqual(hash1);
+    });
+
+    it("returns a new hash when a new endpoint is added", () => {
+      const file = createProjectFromExistingSourceFile(
+        `${__dirname}/__spec-examples__/contract.ts`
+      ).file;
+
+      const { contract } = parseContract(file).unwrapOrThrow();
+
+      const hash0 = hashContract(contract);
+
+      const endpointDefinition: Endpoint = {
+        name: "testEndpoint",
+        description: "test endpoint",
+        tags: ["test"],
+        method: "GET",
+        path: "/test",
+        request: {
+          headers: [],
+          pathParams: [],
+          queryParams: [],
+          body: {
+            type: {
+              kind: TypeKind.STRING
+            }
+          }
+        },
+        responses: []
+      };
+
+      contract.endpoints.push(endpointDefinition);
+
+      const hash1 = hashContract(contract);
+
+      expect(hash0).not.toEqual(hash1);
+    });
+  });
+});
---
+++ lib/src/neu/checksum/hash.ts
@@ -0,0 +1,10 @@
+import { createHash } from "crypto";
+import { Contract } from "../definitions";
+
+export function hashContract(contract: Contract): string {
+  const contractDefinitionString = JSON.stringify(contract);
+
+  return createHash("sha1")
+    .update(contractDefinitionString)
+    .digest("hex");
+}
