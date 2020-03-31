c09b9e6ee45e7aac28682f7a87a0db2f643f8c1b
Clinton Feng <clinton.feng@airtasker.com>
2020-03-31 12:34:28 +1100

    Add unit test to test the case when we have multiple errors and warnings

--- lib/src/linting/find-lint-violations.spec.ts
+++ lib/src/linting/find-lint-violations.spec.ts
@@ -174,4 +174,50 @@ describe("find lint violations", () => {
       expect(warnMock).not.toBeCalled();
     });
   });
+
+  describe("when there are multiple errors and warnings", () => {
+    beforeEach(() => {
+      const groupedLintErrors = [
+        {
+          name: "error1",
+          violations: [{ message: "error_msg" }]
+        },
+        {
+          name: "error2",
+          violations: [{ message: "error_msg" }]
+        },
+        {
+          name: "error3",
+          violations: [{ message: "error_msg" }]
+        },
+        {
+          name: "warn1",
+          violations: [{ message: "warn_msg" }]
+        },
+        {
+          name: "warn2",
+          violations: [{ message: "warn_msg" }]
+        },
+      ];
+
+      const spotConfig = {
+        rules: {
+          warn1: "warn",
+          warn2: "warn"
+        }
+      };
+
+      findLintViolationsResult = findLintViolations(groupedLintErrors, spotConfig, {
+        error: errorMock,
+        warn: warnMock
+      });
+    });
+
+    it("should trigger 3 errors and 2 warnings", () => {
+      expect(findLintViolationsResult.errorCount).toBe(3);
+      expect(findLintViolationsResult.warningCount).toBe(2);
+      expect(errorMock).toBeCalledTimes(3);
+      expect(warnMock).toBeCalledTimes(2);
+    });
+  });
 });
